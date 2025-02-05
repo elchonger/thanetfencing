33f7c6c59aa651663f66c7aeb37ebd515c4fa765
elchonger <lukeattack@hotmail.com>
2025-02-05 16:19:32 +0000

    First push

---
+++ index.php
@@ -0,0 +1,419 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta property="og:title" content="Thanet fencing - The south east's premier fencing and patio contracting company.">
+        <meta property="og:description" content="We supply and fit a full range of fencing, decking and patio solutions throughout Kent.">
+        <meta property="og:image" content="https://thanetfencing.co.uk/assets/img/block-paving%20-and-patios.jpg">
+        <meta property="og:url" content="https://thanetfencing.co.uk/">
+        <meta name="twitter:title" content="Thanet fencing - The south east's premier fencing and patio contracting company.">
+        <meta name="twitter:description" content="We supply and fit a full range of fencing, decking and patio solutions throughout Kent.">
+        <meta name="twitter:image" content="https://thanetfencing.co.uk/assets/img/block-paving-and-patios.jpg">
+        <meta name="twitter:card" content="summary_large_image">
+        
+        <title>Thanet fencing - The south east's premier fencing and patio contracting company.</title>
+        <link rel="shortcut icon" type="image/png" href="favicon.png"/>
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
+        <!-- Core theme CSS (includes Bootstrap)-->
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <!-- My styles yo-->
+        <link href="css/styles-min.css" rel="stylesheet" />
+        <!-- Gallery-->
+        <link rel="stylesheet" href="css/blueimp-gallery.min.css" />
+        <!-- cookiealert styles -->
+        <link rel="stylesheet" href="css/cookiealert.css">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-56394333-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-56394333-1');
+</script>
+
+    </head>
+    <body id="page-top">
+        <!--back to top button-->
+        <button onclick="topFunction()" id="myBtn" title="Go to top"> 
+    <i class="fas fa-chevron-up"></i>
+ </button>
+   
+        
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+          
+     
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">THANET FENCING<!--<img class="logo" src="assets/tf-logo3.png">--></a>
+                
+              
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+            
+
+                 
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#ourwork">Our work</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+       
+        </nav>
+       
+        <!-- Masthead-->
+            <header class="masthead">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                         <h1 class="mx-auto my-0 mb-5">Fencing - Landscaping - Patios</h1>
+                        <!--<h2 class="text-white-50 mx-auto mt-2 mb-5">Repair, Renew, Replace</h2>-->
+                        <a class="btn btn-primary" href="#contact">Contact us for a free quote</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About -->
+        <section class="projects-section bg-light" id="about">
+            <div class="container px-4 px-lg-5 col-lg-8">
+                <h2>About</h2>
+                    <div class="row gx-0 justify-content-center">
+               <p>Based in Westgate Kent, Thanet Fencing operates in the south east.</p>
+
+ <p>Established over 20 years ago, this professional firm ensures a reliable, experienced and trustworthy service. We ensure a high standard of work, quality materials and flexibility to meet the individual needs of the customers in a short timescale.</p>
+
+ <p>Thanet Fencing has a wealth of fencing, landscaping and patio laying skills. These range from large scale commercial projects to the smaller more personal residential works.
+
+ <p>Thanet Fencing has a proven background of accomplishment and success in the trade sector. We pride ourselves on the quality of our work and maintain high professional standards no matter the job.</p>
+                
+               
+                    </div>
+            </div>
+        </section>
+       
+        <section class="projects-section our-work bg-light" id="ourwork">
+            <div class="container px-4 px-lg-5 col-lg-8">
+            <h2>Our work</h2>
+            
+         
+               
+        <div id="links">
+              <a
+                   title="Six foot wooden fence - Thanet Fencing"
+                href="assets/img/fencing-11.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/fencing-11.webp"
+                      }
+                    ]'
+                >
+                </a>
+              <a
+                   title="Before and after paving and steps - Thanet Fencing"
+                href="assets/img/before-and-after-patio.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/before-and-after-patio.webp"
+                      }
+                    ]'
+                >
+                </a>
+              <a
+                   title="Expertly fitted pation with summer house - Thanet Fencing"
+                href="assets/img/patio-with-summer-house.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/patio-with-summer-house.webp"
+                      }
+                    ]'
+                >
+                </a>
+              <a
+                   title="Before and after image of 6 foot fencing - Thanet Fencing"
+                href="assets/img/before-and-after-fencing.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/before-and-after-fencing.webp"
+                      }
+                    ]'
+                >
+                </a>
+              <a
+                   title="Block paved driveway - Thanet Fencing"
+                href="assets/img/driveways.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/driveways.webp"
+                      }
+                    ]'
+                >
+                </a>
+   
+                <a
+                   title="Modern block paving expertly installed - Thanet Fencing"
+                href="assets/img/block-paving-and-patios.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/block-paving-and-patios.webp"
+                      }
+                    ]'
+                >
+                </a>
+                <a
+                   title="Bespoke wooden decking fitted - Thanet Fencing"
+                href="assets/img/decking.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/decking.webp"
+                      }
+                    ]'
+                >
+                </a>
+                 <a
+                   title="Six foot wooden panel fencing with gate - Thanet Fencing"
+                href="assets/img/fencing-and-gates.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/fencing-and-gates.webp"
+                      }
+                    ]'
+                >
+                </a>
+                 <a
+                   title="Before and after image of six foot panel fencing - Thanet Fencing"
+                href="assets/img/fencing.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/fencing.webp"
+                      }
+                    ]'
+                >
+                </a>
+                    <a
+                   title="Before and after image of custom fencing installation - Thanet Fencing"
+                href="assets/img/fencing2.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/fencing2.webp"
+                      }
+                    ]'
+                >
+                </a>
+                    <a
+                   title="Before and after image of wooden fence extention - Thanet Fencing"
+                href="assets/img/fencing3.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/fencing3.webp"
+                      }
+                    ]'
+                >
+                </a>
+                    <a
+                   title="Before and after image of six foot fencing replacement - Thanet Fencing"
+                href="assets/img/fencing4.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/fencing4.webp"
+                      }
+                    ]'
+                >
+                </a>
+                    <a
+                   title="Before and after image of bespoke garden gate expertly fitted - Thanet Fencing"
+                href="assets/img/gate.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/gate.webp"
+                      }
+                    ]'
+                >
+                </a>
+                    <a
+                   title="Garden gate with six foot fencing - Thanet Fencing"
+                href="assets/img/gate2.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/gate2.webp"
+                      }
+                    ]'
+                >
+                </a>
+           
+                    <a
+                   title="Four stages of patio installation - Thanet Fencing"
+                href="assets/img/patio.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/patio.webp"
+                      }
+                    ]'
+                >
+                </a>
+                    <a
+                   title="Four stages of patio being layed - Thanet Fencing"
+                href="assets/img/patio2.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/patio2.webp"
+                      }
+                    ]'
+                >
+                </a>
+                    <a
+                   title="Before and after image of driveway - Thanet Fencing"
+                href="assets/img/paving.jpg"
+                   data-sources='[  {
+                        "type": "image/webp",
+                        "srcset": "assets/img/paving.webp"
+                      }
+                    ]'
+                >
+                </a>
+            </div>
+                </div>
+
+      
+    
+ 
+              <!-- Gallery controls-->          
+                 <div
+  id="blueimp-gallery-carousel"
+  class="blueimp-gallery blueimp-gallery-carousel"
+  aria-label="image carousel"
+>
+  <div class="slides" aria-live="off"></div>
+  <h3 class="title"></h3>
+  <a
+    class="prev"
+    aria-controls="blueimp-gallery-carousel"
+    aria-label="previous slide"
+  ></a>
+  <a
+    class="next"
+    aria-controls="blueimp-gallery-carousel"
+    aria-label="next slide"
+  ></a>
+  <a
+    class="play-pause"
+    aria-controls="blueimp-gallery-carousel"
+    aria-label="play slideshow"
+    aria-pressed="true"
+    role="button"
+  ></a>
+  <ol class="indicator"></ol>
+</div>
+           <!-- Gallery controls end-->   
+        </section>
+              
+ 
+        <!-- Contact-->
+        <section class="contact-section bg-black" id="contact">
+            <div class="container px-4 px-lg-5 col-lg-8">
+                <h2 class="text-white">Contact</h2>
+            <div class="row gx-4 gx-lg-5">
+                      
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50"><a href="mailto:thanetfencing@mail.com">thanetfencing@mail.com</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Phone</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">Mobile: <a href="tel:07946165787">07946 165787</a><br />
+                                Office: <a href="tel:01843833358">01843 833358</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                 <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-heart text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Social</h4>
+                                <hr class="my-4 mx-auto" />
+                          
+                    <a class="mx-2" href="https://instagram.com/thanetfencing"><i class="fab fa-instagram"></i></a>
+                    <a class="mx-2" href="https://www.facebook.com/Thanet-Fencing-107118220706747/"><i class="fab fa-facebook-f"></i></a>
+                   
+             
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+            
+        </section>
+        <!-- START Bootstrap-Cookie-Alert -->
+<div class="alert text-center cookiealert" role="alert">
+    <b>To improve your experience, this website uses cookies. Click accept to proceed or read more to view details about the cookies we use. 
+
+    <button type="button" class="btn btn-primary btn-sm acceptcookies">
+        Accept
+        </button> <a class="cookie-link" href="https://en.wikipedia.org/wiki/General_Data_Protection_Regulation" target="_blank">read more</a></b>
+</div>
+<!-- END Bootstrap-Cookie-Alert -->
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Thanet Fencing <?php echo date("Y"); ?></div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/back-to-top.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <script src="js/blueimp-gallery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.js"></script>
+  
+
+<script>
+  blueimp.Gallery(document.getElementById('links').getElementsByTagName('a'), {
+    container: '#blueimp-gallery-carousel',
+    carousel: true
+  })
+</script>
+        <script>
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
+
+    </body>
+</html>
